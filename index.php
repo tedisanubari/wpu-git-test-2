01b89cab425a2f051d696368a932720cf147d95d
Tedi Sanubari <tedisanubari1719@gmail.com>
2020-04-06 07:22:46 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ducimus sed asperiores corrupti at illo architecto fuga. Placeat libero in omnis, possimus eveniet aliquam? Rem quam itaque molestias eos voluptatem!</p>
 </body>
 
 </html>
